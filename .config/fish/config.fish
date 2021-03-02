93f7c1c3a7728bbd8876886dbd42e8c07f4b32a7
elkrien <78103450+elkrien@users.noreply.github.com>
2021-03-02 09:48:27 +0100

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,31 +1,134 @@
-#### NO GREETING WHEN START
+#### NO GREETING WHEN START ####
 
 function fish_greeting
 end
 
-#### FISH COLORS
+#### MICRO COLORS ####
 
 export MICRO_TRUECOLOR=1
 
-### ZOXIDE
+#### ZOXIDE ####
 
 zoxide init fish | source
-
 
 #### "BAT" AS A MANPAGER
 
 set -x MANPAGER "sh -c 'col -bx | bat -l man -p'" 
 
+#### SPARK ####
 
-#### ALIAS
+set -g spark_version 1.0.0
 
-alias cat="bat"
-alias ls="exa -abghHlS --group-directories-first"
-alias update="sudo pacman -Syu"
-alias df="duf"
+complete -xc spark -n __fish_use_subcommand -a --help -d "Show usage help"
+complete -xc spark -n __fish_use_subcommand -a --version -d "$spark_version"
+complete -xc spark -n __fish_use_subcommand -a --min -d "Minimum range value"
+complete -xc spark -n __fish_use_subcommand -a --max -d "Maximum range value"
 
-#### FISH AUTOSTART
+function spark -d "sparkline generator"
+    if isatty
+        switch "$argv"
+            case {,-}-v{ersion,}
+                echo "spark version $spark_version"
+            case {,-}-h{elp,}
+                echo "usage: spark [--min=<n> --max=<n>] <numbers...>  Draw sparklines"
+                echo "examples:"
+                echo "       spark 1 2 3 4"
+                echo "       seq 100 | sort -R | spark"
+                echo "       awk \\\$0=length spark.fish | spark"
+            case \*
+                echo $argv | spark $argv
+        end
+        return
+    end
+
+    command awk -v FS="[[:space:],]*" -v argv="$argv" '
+        BEGIN {
+            min = match(argv, /--min=[0-9]+/) ? substr(argv, RSTART + 6, RLENGTH - 6) + 0 : ""
+            max = match(argv, /--max=[0-9]+/) ? substr(argv, RSTART + 6, RLENGTH - 6) + 0 : ""
+        }
+        {
+            for (i = j = 1; i <= NF; i++) {
+                if ($i ~ /^--/) continue
+                if ($i !~ /^-?[0-9]/) data[count + j++] = ""
+                else {
+                    v = data[count + j++] = int($i)
+                    if (max == "" && min == "") max = min = v
+                    if (max < v) max = v
+                    if (min > v ) min = v
+                }
+            }
+            count += j - 1
+        }
+        END {
+            n = split(min == max && max ? "▅ ▅" : "▁ ▂ ▃ ▄ ▅ ▆ ▇ █", blocks, " ")
+            scale = (scale = int(256 * (max - min) / (n - 1))) ? scale : 1
+            for (i = 1; i <= count; i++)
+                out = out (data[i] == "" ? " " : blocks[idx = int(256 * (data[i] - min) / scale) + 1])
+            print out
+        }
+    '
+end
+
+function letters
+    cat $argv | awk -vFS='' '{for(i=1;i<=NF;i++){ if($i~/[a-zA-Z]/) { w[tolower($i)]++} } }END{for(i in w) print i,w[i]}' | sort | cut -c 3- | spark | lolcat
+    printf  '%s\n' 'abcdefghijklmnopqrstuvwxyz'  ' ' | lolcat
+end
+
+function commits
+    git log --author="$argv" --format=format:%ad --date=short | uniq -c | awk '{print $1}' | spark | lolcat
+end
+
+
+#### USE !! and !$ IN FISH ####
+
+function __history_previous_command
+  switch (commandline -t)
+  case "!"
+    commandline -t $history[1]; commandline -f repaint
+  case "*"
+    commandline -i !
+  end
+end
+
+function __history_previous_command_arguments
+  switch (commandline -t)
+  case "!"
+    commandline -t ""
+    commandline -f history-token-search-backward
+  case "*"
+    commandline -i '$'
+  end
+end
+
+# The bindings for !! and !$
+  bind ! __history_previous_command
+  bind '$' __history_previous_command_arguments
+
+#### ALIASES ####
+
+# Spark aliases
+alias clear='clear; echo; echo; seq 1 (tput cols) | sort -R | spark | lolcat; echo; echo'
+
+# ls change to exa
+alias ls='exa -al --color=always --group-directories-first' # standard listing
+alias la='exa -a --color=always --group-directories-first'  # all files and dirs
+alias ll='exa -l --color=always --group-directories-first'  # long format
+alias lt='exa -aT --color=always --group-directories-first' # tree listing
+
+# cat change to bat
+alias cat='bat'
+
+# update system
+alias update='sudo pacman -Syu'
+
+# df change to duf
+alias df='duf'
+
+#### FISH AUTOSTART ####
 neofetch
+
+#### INIT STARSHIP ####
+
 starship init fish | source
 
 
